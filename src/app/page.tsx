bf8ab965c089d0b3acda822b2e9b87d543f74965
ArpDark <aritrapal073@gmail.com>
2024-03-05 12:39:45 +0530

    mobile navbar is completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 "use client"
 
+import React,{useState,Fragment} from "react";
+import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
-    <div className="">
-
+    <div className="flex flex-col relative min-h-screen h-fit overflow-y-auto bg-[#eff2f5]">
+      <Navbar/>
+      <div className="">
+        <p className="text-[#3e5765]">Cryptocurrencies</p>
+        <p className="text-[#0f1629]">Bitcoin</p>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,63 @@
+"use client"
+
+import React,{useState,Fragment} from "react";
+import MenuIcon from '@mui/icons-material/Menu';
+import { Menu, Transition } from '@headlessui/react';
+
+
+export default function Navbar(){
+    return (
+        <div className="flex  w-full h-16 bg-white drop-shadow-[0_0_12px_rgba(16, 38, 73, 0.06)] items-center md:justify-normal z-10 ">
+        <div className="hidden md:visible md:flex w-1/2 justify-around border-2 border-yellow-300 h-fit ">
+          <a href="#" className=" font-semibold text-base">Crypto Taxes</a>
+          <a href="#" className=" font-semibold text-base">Free Tools</a>
+          <a href="#" className=" font-semibold text-base">Resource Center</a>
+          <button className=" font-semibold text-base">Get Started</button>
+        </div>
+
+        <div className="md:hidden w-full flex items-center">
+          <img src="./logo.png" className="w-[81px] h-[20px] border-2 border-red-400 ml-[24px]" alt="logo" />
+          <Menu as="div" className="flex text-left justify-end justify-self-end w-full border-2 border-green-400 mr-[24px]">
+            <div >
+              <Menu.Button className="inline-flex w-full justify-center bg-transparent ">
+                <MenuIcon sx={{fontSize:27 }}/>
+              </Menu.Button>
+            </div>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute z-10 mt-14 ml-16 w-5/6  origin-top-right bg-white shadow-lg ">
+                <div className="">
+                  <Menu.Item>
+                      <a href="#home" className=" text-black block px-4 py-2 text-sm">
+                        Crypto Taxes
+                      </a>
+                  </Menu.Item>
+                  <Menu.Item>
+                      <a href="#about" className=" text-black block px-4 py-2 text-sm">
+                        Free Tools
+                      </a>
+                  </Menu.Item>
+                  <Menu.Item>
+                      <a href="#" className=" text-black block px-4 py-2 text-sm">
+                        Resource Center
+                      </a>
+                  </Menu.Item>
+                  <Menu.Item>
+                    <button className="">Get Started</button>
+                  </Menu.Item>
+                  
+                </div>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+        </div>
+      </div>
+    );
+}
